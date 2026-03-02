ec51925e4a62a1944d1b6b8c8fe090311a372d2f
Lombardo Neuman <100061697@csisd.org>
2026-03-02 08:59:28 -0600

    Video 9 Checkpoint

--- Assets/Scripts/Scene01/Scene01Events.cs
+++ Assets/Scripts/Scene01/Scene01Events.cs
@@ -10,7 +10,18 @@ public class Scene01Events : MonoBehaviour
     public GameObject textBox;
 
     [SerializeField] AudioSource girlSigh;
-    [SerializeField] AudioSource girlGasp;    
+    [SerializeField] AudioSource girlGasp;
+
+
+    [SerializeField] string textToSpeak;
+    [SerializeField] int currentTextLength;
+    [SerializeField] int textLength;
+    [SerializeField] GameObject mainTextObject;
+
+    void Update()
+    {
+        textLength = TextCreator.charCount;
+    }
 
     void Start()
     {
@@ -24,16 +35,23 @@ public class Scene01Events : MonoBehaviour
         charKasumi.SetActive(true);
         yield return new WaitForSeconds(2);
         // this is where our text function will go in future tutorial
-        textBox.SetActive(true);
+        mainTextObject.SetActive(true);
+        textToSpeak = "I wonder where Haruka has got to. She was supposed to be here.";
+        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+        currentTextLength = textToSpeak.Length;
+        TextCreator.runTextPrint = true;
         girlSigh.Play();
+        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => textLength == currentTextLength);
+        yield return new WaitForSeconds(0.05f);
+
+
+
+        textBox.SetActive(true);
         yield return new WaitForSeconds(2);
         charHaruka.SetActive(true);
         girlGasp.Play();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
