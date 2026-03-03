29d7cab0be06a43f240cbb820898f680850b9626
Lombardo Neuman <100061697@csisd.org>
2026-03-03 08:49:18 -0600

    Video 10 Checkpoint

--- Assets/Scripts/Scene01/Scene01Events.cs
+++ Assets/Scripts/Scene01/Scene01Events.cs
@@ -8,17 +8,15 @@ public class Scene01Events : MonoBehaviour
     public GameObject charKasumi;
     public GameObject charHaruka;
     public GameObject textBox;
-
     [SerializeField] AudioSource girlSigh;
     [SerializeField] AudioSource girlGasp;
-
-
     [SerializeField] string textToSpeak;
     [SerializeField] int currentTextLength;
     [SerializeField] int textLength;
     [SerializeField] GameObject mainTextObject;
     [SerializeField] GameObject nextButton;
     [SerializeField] int eventPos = 0;
+    [SerializeField] GameObject charName;
 
     void Update()
     {
@@ -32,6 +30,7 @@ public class Scene01Events : MonoBehaviour
 
     IEnumerator EventStarter()
     {
+        // event 0
         yield return new WaitForSeconds(2);
         fadeScreenIn.SetActive(false);
         charKasumi.SetActive(true);
@@ -46,14 +45,39 @@ public class Scene01Events : MonoBehaviour
         yield return new WaitForSeconds(0.05f);
         yield return new WaitForSeconds(1);
         yield return new WaitUntil(() => textLength == currentTextLength);
-        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(0.5f);
         nextButton.SetActive(true);
+        eventPos = 1;
 
-
-        //textBox.SetActive(true);
-        //yield return new WaitForSeconds(2);
-        //charHaruka.SetActive(true);
-        //girlGasp.Play();
     }
+
+    IEnumerator EventOne()
+    {
+        // event 1
+        nextButton.SetActive(false);
+        charHaruka.SetActive(true);
+        textBox.SetActive(true);
+        charName.GetComponent<TMPro.TMP_Text>().text = "Haruka";
+        textToSpeak = "I was hiding over in the corner.";
+        textBox.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+        currentTextLength = textToSpeak.Length;
+        TextCreator.runTextPrint = true;
+        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => textLength == currentTextLength);
+        yield return new WaitForSeconds(0.5f);
+        girlGasp.Play();
+        nextButton.SetActive(true);
+        eventPos = 2;
+    }
+
+    public void NextButton()
+    {
+        if (eventPos == 1)
+        {
+            StartCoroutine(EventOne());
+        }
+    }
+
 
 }
