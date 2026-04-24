610bce8bcb5d1a38be076d4573b1819397168f8d
Lombardo Neuman <100061697@csisd.org>
2026-04-24 09:14:47 -0500

    Changed Calendar and Added BlueHour Transition

---
+++ Assets/Scripts/BlueHourTransit16/BlueHourTransit16.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EnableClocksAndAudio : MonoBehaviour
+{
+    public GameObject clocks;        // Drag your Clocks object here
+    public AudioSource audioSource;  // Drag AudioSource here
+    public AudioClip tickingSFX;     // Assign ticking sound here
+
+    void Start()
+    {
+        StartCoroutine(Sequence());
+    }
+
+    IEnumerator Sequence()
+    {
+        // Wait 1 second
+        yield return new WaitForSeconds(1f);
+
+        // Enable Clocks
+        if (clocks != null)
+        {
+            clocks.SetActive(true);
+        }
+
+        // Play ticking sound
+        if (audioSource != null && tickingSFX != null)
+        {
+            audioSource.clip = tickingSFX;
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("AudioSource or ticking SFX not assigned!");
+        }
+
+        // Wait remaining 7 seconds (total 8)
+        yield return new WaitForSeconds(7f);
+
+        // Load scene 7
+        SceneManager.LoadScene(7);
+    }
+}
